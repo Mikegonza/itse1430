a44efe3277b548913f39e8305daaf0e7c27a3cc4
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2022-01-25 21:44:31 -0600

    demo

---
+++ MovieLib.consoleHost/MovieLib.consoleHost/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MovieLib.consoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
