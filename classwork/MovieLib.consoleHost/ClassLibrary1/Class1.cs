57f2fde91ac3988505475605afc9188ca3f88539
Miguel Angel Gonzalez Mauricio <miguelangel.gonzalezmaur@my.tccd.edu>
2022-03-01 21:25:14 -0600

    new

--- classwork/MovieLib.consoleHost/ClassLibrary1/Class1.cs
+++ classwork/MovieLib.consoleHost/ClassLibrary1/Class1.cs
@@ -1,79 +1,138 @@
 ﻿using System;
 
 namespace MovieLib
-
 {
-    // class- wraps data and functionatily
-    // naming nouns, pascal cased
-    //default accesibility: internal for a class private for a class member 
-    /// <summary> represnet a movie.</summary>summary>
-    
+    // Class - wraps data and functionality
+    //   Naming: nouns, Pascal cased
+    //   Default accessibility: internal for a class, private for a class member
+
+    /// <summary>Represents a movie.</summary>
     public class Movie
     {
-        //acces modifiers
-        //public- everyone
-        //private-declaring type
-        //internal- assembly only
+        //Access modifiers
+        //  public - everyone
+        //  internal - assembly only
+        //  private - declaring type
 
-        // fields where the data is stored
-        //naming nouns, camel cased
-        //readeable and writable (assuming accesibility)
-        //work just like al other variables
-        /// 
-         public string Tittle
+        //Fields - where the data is stored
+        // Naming: nouns, camel cased, start with underscore
+        //   Readable and writable (assuming accessibility)
+        //   Work just like all other variables
+        // Do not expose publicly
+        public const int MinimumReleaseYear = 1900;
+        public readonly DateTime MinimumReleaseDate = new DateTime(1900, 1, 1);
+
+
+        //Properties - exposes data
+        //  Field syntax with methods being called
+        //  Can get (read) and/or set (write)
+        //    T getter ()
+        //    void setter ( T value )
+
+        // Handling null
+        //   null coalescing ::= E ?? E, find first non-null
+        //   null conditional ::= E?.M, execute M if E not null, changes type to T?
+        //   combined ::= E?.M ?? D, resets type back to T            
+
+        /// <summary>Gets or sets the title of the movie.</summary>
+        public string Title
         {
-            get {return _title ; }
-            set { _title = value; }
+            //get { return !String.IsNullOrEmpty(_title) ? _title : ""; }
+            //get { return (_title != null) ? _title : ""; }
+            get { return _title ?? ""; }
+
+            //set { _title = (value != null ) ? value.Trim() : null; }
+            //set { _title = (value ?? "").Trim(); }
+            set { _title = value?.Trim(); }
         }
+        private string _title;
 
+        //Auto property syntax when property is simply reading/writing backing field
+        /// <summary>Gets or sets the duration in minutes.</summary>
+        public int Duration { get; set; }
 
-         private string _title;
-         public int _duration;
-         public int _releaseYear = 1900;
-         public string _rating;
-         public string _genre;
-         public bool _isClassic;
-         public string _description;
+        public int ReleaseYear { get; set; } = 1900;
+        //{
+        //    get { return _releaseYear; }
+        //    set { _releaseYear = value; }
+        //}
+        //private int _releaseYear = 1900;
 
+        public string Rating
+        {
+            get { return !String.IsNullOrEmpty(_rating) ? _rating : ""; }
+            set { _rating = value; }
+        }
+        private string _rating;
 
+        public string Genre
+        {
+            get { return !String.IsNullOrEmpty(_genre) ? _genre : ""; }
+            set { _genre = value; }
+        }
+        private string _genre;
+
+        public bool IsClassic { get; set; }
+        
+        public string Description
+        {
+            get { return !String.IsNullOrEmpty(_description) ? _description : ""; }
+            set { _description = value; }
+        }
+        private string _description;
+
+        //Calculated property := BW <= 1939
+        // No setter so it cannot be written to
         public bool IsBlackAndWhite
         {
-            get { return _isBlackAndWhite; }
-            set { }
+            get { return ReleaseYear <= 1939; }
+            //set { }
         }
+        //private bool _isBlackAndWhite;
 
-        private bool _isBlackAndWhite;
-        // all instace methods have a hidden this parameter that represents the instance
-        ///<summary>Validates the instance.</summary>
-        /// <returns>Return error message if any or empty string otherwise. </returns>
+        //private void CalculateBlackAndWhite ()
+        //{
+        //    _isBlackAndWhite = ReleaseYear <= 1939;
+        //}
 
-        public void CalculateBlackAndWhite()
+        //All instance methods have a hidden this parameter that represents the instance
+        /// <summary>Validates the instance.</summary>
+        /// <returns>Returns error message if any or empty string otherwise.</returns>
+        public string Validate ( /* Movie this */ )
         {
-            isBlackAndWhite= releaseYear <= 1939;
+            //var now = DateTime.now;
 
+            //var title = "";
+
+            //Title is required
+            if (String.IsNullOrEmpty(_title))
+                return "Title is required";
+
+            if (Duration < 0)
+            //if (this.duration < 0)
+                return "Duration must be at least 0";
+
+            if (ReleaseYear < MinimumReleaseYear)
+                return $"Release Year must be at least {MinimumReleaseYear}";
+
+            if (String.IsNullOrEmpty(Rating))
+                return "Rating is required";
+
+            //Special rule - no classic movies before 1940
+            if (IsClassic && ReleaseYear < 1940)
+                return "Release Year must be at least 1940 to be a classic";
+
+            return "";
         }
 
-        public string Validate(/* movie this*/)
-        {
-            var tittle="";
-
-            if (String.IsNullOrEmpty(_tittle))
-                return "tittle is required";
-            if (_duration<0)
-                return "duration must be a least 0";
-            if (_releaseYear < 1900)
-                return "Release must be a 1900";
-            if (String.IsNullOrEmpty(_genre) )
-                return "Genre is required";
-            //special rule - no classoic movie before 1940
-            if (_isClassic $$ _releaseyear < 1940)
-                return " release year must be at least 1940 to be a classic";
-
-            return"";
-
-        }
-
-        private int id;
-        
+        public int Id { get; private set; }
+        //{
+        //    //Mixed acessibility
+        //    // Only on either getter or setter
+        //    // Must be more restrictive than property
+        //    get { return _id; }
+        //    private set { _id = value; }
+        //}
+        //private int _id;
     }
 }
--- classwork/MovieLib.consoleHost/MovieLib.consoleHost/Program.cs
+++ classwork/MovieLib.consoleHost/MovieLib.consoleHost/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿/*
+ * Classwork
+ * ITSE 1430 
+ */
+using System;
 
-namespace MovieLib.consoleHost
+namespace MovieLib.ConsoleHost
 {
     class Program
     {
@@ -8,140 +12,226 @@ namespace MovieLib.consoleHost
         static void Main ( string[] args )
         {
             //Block style declaration - all variables declared at top of function
-            //on demand / inline declaration - variables are declared as needed
+            //On demand/inline declaration - variables are declared as needed
             var done = false;
-
             do
             {
                 char input = DisplayMenu();
 
+                #region Comments
 
                 //Handle input
-                // if (input== 'A')
-                //     AddMovie();
-                // else if (input =='V')
+                //if (input == 'A')
+                //    AddMovie();
+                //else if (input == 'V')
                 //    ViewMovie();
-                // else if (input =='Q')
-                //     if (confirmQuit())
-                //         break; //exits the loop
+                //else if (input == 'Q')
+                //    if (ConfirmQuit())
+                //        break;  //Exits the loop
+
+                //Switch statement replaces a series of if-else-if where a single expression
+                // is compared to constant values
+                // - Expression can be any expression type
+                // - Case labels must be:
+                //     - Compile time constants
+                //     - Unique within the statement
+                // - Fallthrough is not allowed in C# unless the previous case statement is empty
+                // - Block statement should be used for multiple statements but
+                //   because of compilation issues is not required in many cases
+                //   Most developers use block statement when there is more than 1 statement excluding break
+                #endregion
                 switch (input)
                 {
-                    case 'a':
+                    case 'a':   //Fallthrough allowed when case statement is empty
                     case 'A': AddMovie(); break;
+
                     case 'v':
                     case 'V': ViewMovie(); break;
+
                     case 'd':
                     case 'D': DeleteMovie(); break;
+
                     case 'q':
                     case 'Q':
                     {
-                        if (confirmQuit())
+                        if (ConfirmQuit())
                             done = true;
+
                         break;
                     };
                     default: Console.WriteLine("Unknown option"); break;
                 };
             } while (!done);
         }
-        private static void ViewMovie ()
+
+        //Display the main menu
+        static char DisplayMenu ()
         {
-            //ToDo movie exist
-            //if (String.IsNullOrEmpty(movie.title)) 
-            if (movie==null)
-            {
-                Console.WriteLine("no movie to view");
-                return;
-            }
-            Console.WriteLine(movie.title);
-            // releaseYear (run length: duration minutes) rating
-            //formatin 1 string concatenation
-            //Console.WriteLine (releaseYear + " (" + duration + " mins) " + rating);
+            Console.WriteLine("Movie Library");
+            //Console.WriteLine("--------------");
+            Console.WriteLine("".PadLeft(20, '-'));
+            Console.WriteLine("A)dd Movie");
+            Console.WriteLine("V)iew Movie");
+            Console.WriteLine("E)dit Movie");
+            Console.WriteLine("D)elete Movie");
+            Console.WriteLine("Q)uit");
 
-            // formating 2 string contatenatinon
-            //Console.WriteLine("{0} ({1}mins) {2}", releaseYear, duration, rating);
-            //string temp = String.Format("{0} ({1}mins) {2}", releaseYear, duration, rating);
-            //Console.WriteLine(temp);
-
-            //formating 3 string interpolation
-            Console.WriteLine($"{movie.releaseYear } ({movie.duration} mins) {movie.rating}");
-
-            //genre (color black white)
-            //Console.WriteLine(genre + " (  + isColor" + ")");
-            // Console.WriteLine($"{genre} ({isColor})");
-            //if (isColor)
-            //  Console.WriteLine($"{genre} (color)");
-            //else
-            //Console.WriteLine($"{ genre} (Black and white)");
-            // conditional operator
-            Console.WriteLine($"{movie.genre} ({(movie.Classic ? "Classic" : "Black and White")})");
-
-            Console.WriteLine(movie.releaseYear);
-            Console.WriteLine(movie.duration);
-            Console.WriteLine(movie.isClassic);
-            Console.WriteLine(movie.description);
-            Console.WriteLine(movie.genre);
-
-        }
-        static bool confirmQuit ()
-        {
-            return ReadBoolean("Are you sure you wnat to quit (Y/N)? ");
-
-        }
-        
-        private static void AddMovie ()
-        { 
             do
             {
-           
+                string input = Console.ReadLine();
+
+                //Validate input, case insensitive
+                if (String.Compare(input, "A", true) == 0)
+                    return 'A';
+                else if (String.Compare(input, "V", true) == 0)
+                    return 'V';
+                else if (String.Equals(input, "E", StringComparison.CurrentCultureIgnoreCase))
+                    return 'E';
+                else if (String.Compare(input, "D", true) == 0)
+                    return 'D';
+                else if (String.Compare(input, "Q", true) == 0)
+                    return 'Q';
+                else
+                    Console.WriteLine("Invalid input");
+            } while (true);
+        }
+
+        // Adds a movie
+        private static void AddMovie ()
+        {
             movie = new Movie();
 
-            movie.Title = ReadString("Enter a movie tittle", true);
-            movie.duration = ReadInt32("Enter duration in minutes(>=0):", 0);
-            movie.releaseYear = ReadInt32("Enter the release year:", 1900);
-            movie.rating = ReadString("Enter a rating (e.g PG-13):", true);
-            movie.genre = ReadString("Enter a genre(optional:)", false);
-            movie.isColor = ReadBoolean("Is Classic (Y/N)");
-            movie.description = ReadString("Enter a description (optional):", false);
-            //movie.isBlackAndWhite= movie.releaseYear <= 1939;
-            movie.CalculateBlackAndWhite();
-            var error = movie.Validate();
-            if (!String.IsNullOrEmpty (error))
-                return;
-            Console.WriteLine (error);
-            }while (true);
+            object val = 10;
+            val = "Hello";
+            val = null;
+            
+            //movie.IsBlackAndWhite = false;
+            do
+            {
+                movie.Title = ReadString("Enter a movie title: ", true);
+                movie.Duration = ReadInt32("Enter duration in minutes (>= 0): ", 0);
+                movie.ReleaseYear = ReadInt32("Enter the release year: ", 1900);
+                movie.Rating = ReadString("Enter a rating (e.g. PG, PG-13): ", true);
+                movie.Genre = ReadString("Enter a genre (optional): ", false);
+                movie.IsClassic = ReadBoolean("Is classic (Y/N)?");
+                movie.Description = ReadString("Enter a description (optional): ", false);
+
+                //movie.isBlackAndWhite = movie.releaseYear <= 1939;
+                //movie.CalculateBlackAndWhite();
+
+                var error = movie.Validate();
+                if (String.IsNullOrEmpty(error))
+                    return;
+
+                Console.WriteLine(error);
+            } while (true);
         }
-        
+
+        // Deletes a movie
         private static void DeleteMovie ()
         {
-
             //if (String.IsNullOrEmpty(movie.title))
-            if (movie==null)
+            if (movie == null)
             {
                 Console.WriteLine("No movie to delete");
                 return;
             };
-            //delete movie
-            if (ReadBoolean($"Are you sure you want to delete the movie '{movie.title}' (Y/N)"))
-                movie= null;
+
+            //Confirm and delete the movie
+            if (ReadBoolean($"Are you sure you want to delete '{movie.Title}' (Y/N)"))
+                movie = null;
+        }
+
+        //View a movie
+        private static void ViewMovie ()
+        {
+            //Does movie exist
+            //if (String.IsNullOrEmpty(movie.title))
+            if (movie == null)
+            {
+                Console.WriteLine("No movie to view");
+                return;
+            };
+
+            Console.WriteLine(movie.Title);
+
+            //Desired format: releaseYear (duration mins) rating
+            
+            //Formatting 1 - string concatenation
+            //  Console.WriteLine(releaseYear + " (" + duration + " mins) " + rating);
+            //Formatting 2 - string formatting
+            //  Console.WriteLine("{0} ({1} mins) {2}", releaseYear, duration, rating);
+            //  string temp = String.Format("{0} ({1} mins) {2}", releaseYear, duration, rating);
+            //  Console.WriteLine(temp);
+            //Formatting 3 - string interpolation
+            Console.WriteLine($"{movie.ReleaseYear} ({movie.Duration} mins) {movie.Rating}");
+            
+            //Conditional operator
+            Console.WriteLine($"{movie.Genre} ({(movie.IsClassic ? "Classic" : "")})");
+            Console.WriteLine(movie.Description);
+        }
+
+        //TODO: Fix these variables to remove warnings
+        static Movie movie;
+        //type checking
+
+        static void Display(object data)
+        {
+            //Assume a string
+            //C-style cast
+            //  runtime error if wrong
+            //  no way to validate at runtime
+            // still compile time safe
+            //(string)10;
+            // Is operator
+            // E is T -> boolen
+            // still need a type cast
+            // as - operatror  ::= E as T ( returns T or null)
+            // only works with T if T is nullable (string, object,class types)
+            // pattern - matching ::= E is T id (boolean with id as typed value if true)
+            // string and object value ty[pe
+            //reference = class
+            //
+            var dataString =(string)data;
+                         
+            if (data is string)
+            { 
+                dataString= (string)data;            
+            };
+
+            //data as int
+            dataString = data as string;
+            if (dataString != null) { };
+
+            // Pattern matching 
+            if (data is string dataString2)
+            {
+            };
 
         }
-        //unit 1 only
-        static  Movie movie;
         
-        static bool ReadBoolean ( string message )
+        #region Helper Functions
 
+        // Get confirmation from user to quit
+        static bool ConfirmQuit ()
         {
+            return ReadBoolean("Are you sure you want to quit (y/n)? ");
+        }
+                
+        // Reads a boolean input from user
+        static bool ReadBoolean ( string message )
+        {            
             Console.Write(message);
+
             do
             {
-                ConsoleKeyInfo Key = Console.ReadKey();
+                ConsoleKeyInfo key = Console.ReadKey(true);
 
-                //Validate
-                if (Key.Key == ConsoleKey.Y)
+                if (key.Key == ConsoleKey.Y)
                 {
-                    Console.WriteLine();
+                    Console.WriteLine('Y');
                     return true;
-                } else if (Key.Key == ConsoleKey.N)
+                } else if (key.Key == ConsoleKey.N)
                 {
                     Console.WriteLine('N');
                     return false;
@@ -149,88 +239,55 @@ namespace MovieLib.consoleHost
             } while (true);
         }
 
-        private static int ReadInt32 ( string message, int minimumValue )
+        // Reads an integral value from user
+        static int ReadInt32 ( string message, int minimumValue )
         {
             Console.Write(message);
+
             while (true)
             {
-                //type inferencing - compiler infers actual type based upon usage
-                //has 0 impact on runtime behavior
+                //Type inferencing - compiler infers actual type based upon usage
+                // Has 0 impact on runtime behavior
                 //string input = Console.ReadLine();
                 var input = Console.ReadLine();
 
-                //TODO: Validate
+                //Validate
                 //int result = Int32.Parse(input);
                 //int result;
                 //if (Int32.TryParse(input, out result))
 
+                //Inline variable declaration - declare and pass a variable to an output parameter
+                //   Identical to above code
                 //if (Int32.TryParse(input, out int result))
-                //   if (result >= minimumValue)
-                //    return result;
-
-                //  Console.WriteLine("Value must be >= " + minimumValue);
+                //    if (result >= minimumValue)
                 if (Int32.TryParse(input, out var result) && result >= minimumValue)
                     return result;
+
                 Console.WriteLine("Value must be >= " + minimumValue);
-
-
             };
-
         }
 
         //Function naming rules
-        //Functions are actions-> verbs
-        //functions are always pascal cased
-        //funtions should do a single, logical thing
+        //  Functions are actions -> verbs
+        //  Functions are always Pascal cased
+        // Functions should do a single, logical thing
 
+        // Reads a(n), optionally required, string from the user
         private static string ReadString ( string message, bool required )
         {
+            Console.WriteLine(message);
+
             do
             {
-                Console.WriteLine(message);
                 string input = Console.ReadLine();
-                //Todo: Validate input, if required
+
+                //Validate if required
                 if (!required || !String.IsNullOrEmpty(input))
                     return input;
-                Console.WriteLine("value is required");
-            }
-            while (true);
 
-
-        }
-        static char DisplayMenu ()
-        {
-            Console.WriteLine("Movie Library");
-            //Console.WriteLine("-----------");
-            Console.WriteLine("".PadLeft(20, '-'));
-            Console.WriteLine("A)dd Movie");
-            Console.WriteLine("V)iew Movie");
-            Console.WriteLine("E)dit Movie");
-            Console.WriteLine("D)elete Movie");
-            Console.WriteLine("Q)uit");
-            do
-            {
-                string input = Console.ReadLine();
-
-                //Validate input
-
-                if (String.Compare(input, "A", true)==0)
-                    return 'A';
-                else if (String.Compare(input, "V", true)==0)
-                    return 'V';
-                else if (String.Equals(input, "E", StringComparison.CurrentCultureIgnoreCase))
-                    return 'E';
-                else if (String.Compare(input, "D", true)==0)
-                    return 'D';
-                else if (String.Compare(input, "Q", true)==0)
-                    return 'Q';
-                else
-                    Console.WriteLine("Invalid Input");
+                Console.WriteLine("Value is required");
             } while (true);
-
         }
+        #endregion
     }
 }
-        
-    
-
